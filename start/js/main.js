42e5c493893f258a6b31dd056145c5f0588a89b8
Anastasiia Serkiz <anastasiaserkiz@gmail.com>
2021-11-23 21:08:36 +0200

    slider adaptive

--- start/js/main.js
+++ start/js/main.js
@@ -3,8 +3,30 @@ $(function(){
           dots: true,
           slidesToShow: 3,
           slidesToScroll: 3,
+          
           nextArrow: '<button type="button" class="slick-btn slick-next"><img type="button" ></button>',
           prevArrow: '<button type="button" class="slick-btn slick-prev"><img type="button" ></button>',
+
+          responsive: [
+            {
+              breakpoint: 1250,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: NamedNodeMap,
+              }
+            },
+
+              
+                {
+                  breakpoint: 840,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll:1,
+                    arrows: NamedNodeMap,
+                  }
+                }
+              ]
           
       });
 
